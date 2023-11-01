29aa867395571f9af4e1b1c2786d75e3e0a32cf7
Maxence <noizetmaxencepro@gmail.com>
2023-11-01 23:01:14 +0100

    fix(lint): update rules and ignored files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   endOfLine: "crlf",
-  printWidth: 100,
-  semi: true,
-  singleQuote: false,
-  organizeImportsSkipDestructiveCodeActions: false,
   plugins: ["prettier-plugin-astro"],
-  tabWidth: 2,
-  trailingComma: "none",
-  useTabs: false
+  printWidth: 100,
+  trailingComma: "none"
 };
