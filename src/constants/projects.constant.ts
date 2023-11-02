445d90de42ac836eca72b541b1012afbfb52de23
Maxence <noizetmaxencepro@gmail.com>
2023-11-02 15:51:42 +0100

    feat(project): create dynamic project page

--- src/constants/projects.constant.ts
+++ src/constants/projects.constant.ts
@@ -6,7 +6,10 @@ export interface IProject {
     intro: string;
     features?: string[];
   };
-  links?: string[];
+  links?: {
+    name: string;
+    href: string;
+  }[];
 }
 
 export const projects: IProject[] = [
@@ -22,12 +25,17 @@ export const projects: IProject[] = [
         "Un Évaluateur peut créer, modifier et supprimer les différentes grilles d'évaluations ainsi que de voir le classement actuel des étudiants. Une Secrétaire a également la possibilité de voir le classement actuel des étudiants et peut également le télécharger en format CSV. Quand à l'Administrateur, il peut créer, modifier et supprimer les comptes utilisateurs de l'application."
       ]
     },
-    links: ["https://github.com/mqxewww/s11-selection"]
+    links: [
+      {
+        name: "Repo GitHub",
+        href: "https://github.com/mqxewww/s11-selection"
+      }
+    ]
   },
   {
     name: "s11-dolibarrapp",
     icons: ["cordova", "tailwindcss", "javascript"],
-    date: "Mar 2021",
+    date: "Mars 2021",
     description: {
       intro:
         "S11 Dolibarrapp est une application mobile Android développé en HTML/JS destinée aux entreprises utilisatrices de l'ERP Dolibarr. Son but est de rendre plus rapide et plus accessible la comptabilité entre entreprises/employés.",
@@ -40,7 +48,7 @@ export const projects: IProject[] = [
   {
     name: "vac-notifications",
     icons: ["html5", "nodejs", "mongodb", "ovh"],
-    date: "May 2021",
+    date: "Mai 2021",
     description: {
       intro:
         "VAC Notifications est une application web type PPMS développée en JS utilisée pour avertir un groupe d'habitants d'un risque météorologique, d'attentat, et autres... Elle a été développé en 6 semaines lors de mon stage de 1re année.",
@@ -59,9 +67,9 @@ export const projects: IProject[] = [
         "S11-Success est une application web développé avec le framework Vue.js. Elle est destinée aux entreprises souhaitant faire passer tout type de questionnaire à ses employés.",
       features: [
         "L'application possède deux types de compte : Administrateur et Employé.",
-        "Les questionnaires sont créés par les administrateurs. Chaque questionnaire est assigné une catégorie, peut avoir une limite de temps, et peut contenir des questions soit à choix unique/mulitples soit à réponse libre. L'administrateur doit également définir un code secret pour limiter l'accès au questionnaire.",
+        "Les questionnaires sont créés par les administrateurs. Chaque questionnaire est assigné une catégorie, peut avoir une limite de temps, et peut contenir des questions soit à choix unique/multiples soit à réponse libre. L'administrateur doit également définir un code secret pour limiter l'accès au questionnaire.",
         "L'utilisateur, une fois connecté, a accès à deux pages. La première est une page de statistiques où il peut consulter son score sur chaque questionnaire passé. La seconde est la page qui lui permet d'accéder à un questionnaire de son choix.",
-        "L'administrateur possédé lui aussi une page statistique où il peut voir la moyenne ainsi que la plus basse & haute note de chacun de ses questionnaires créés. Il est également gérant des comptes.",
+        "L'administrateur possède lui aussi une page statistique où il peut voir la moyenne ainsi que la plus basse & haute note de chacun de ses questionnaires créés. Il est également gérant des comptes.",
         "Pour terminer, l'application possède un système de correction automatique. Toutes les questions à choix uniques et multiples sont corrigées automatiquement. Pour les questions à réponse libre, c'est le créateur du questionnaire qui doit valider ou non la réponse fournie."
       ]
     }
@@ -83,34 +91,43 @@ export const projects: IProject[] = [
       intro:
         "Mon portfolio est le site sur lequel vous vous trouvez actuellement ! ✨ J'y expose mes compétences ainsi que mes différents projets."
     },
-    links: ["https://portfolio.apps.mqxewww.dev"]
+    links: [
+      {
+        name: "Repo GitHub",
+        href: "https://github.com/mqxewww/astro-portfolio"
+      },
+      {
+        name: "Portfolio en ligne",
+        href: "https://portfolio.apps.mqxewww.dev"
+      }
+    ]
   },
   {
     name: "viticulteurs-market",
     icons: ["nextjs", "tailwindcss", "nodejs", "mysql"],
     date: "Nov 2022",
     description: {
-      intro:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae dui congue dui porta volutpat vitae id risus.",
-      features: [
-        "Cras eleifend lacinia velit ac mattis. Nulla nec lectus eget sem tempus iaculis vel in odio. Mauris sollicitudin, risus sed vulputate cursus, lorem urna posuere sem, a consectetur diam augue at ipsum. Aenean ut massa enim.",
-        "Aliquam purus ligula, semper vel pretium non, consectetur in ipsum. Fusce feugiat urna quis magna mollis tempor. Sed vel dui et sapien pretium semper ac in tellus. Curabitur nisl risus, aliquet in enim eu, feugiat mollis mauris."
-      ]
+      intro: ""
     },
-    links: ["https://www.viticulteurs.market"]
+    links: [
+      {
+        name: "Site en ligne",
+        href: "https://www.viticulteurs.market"
+      }
+    ]
   },
   {
     name: "novacollecte",
     icons: ["nextjs", "typescript", "nestjs", "mysql"],
-    date: "Feb 2023",
+    date: "Fév 2023",
     description: {
-      intro:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae dui congue dui porta volutpat vitae id risus.",
-      features: [
-        "Cras eleifend lacinia velit ac mattis. Nulla nec lectus eget sem tempus iaculis vel in odio. Mauris sollicitudin, risus sed vulputate cursus, lorem urna posuere sem, a consectetur diam augue at ipsum. Aenean ut massa enim.",
-        "Aliquam purus ligula, semper vel pretium non, consectetur in ipsum. Fusce feugiat urna quis magna mollis tempor. Sed vel dui et sapien pretium semper ac in tellus. Curabitur nisl risus, aliquet in enim eu, feugiat mollis mauris."
-      ]
+      intro: ""
     },
-    links: ["https://www.agrivitech.com/solutions/novacollecte"]
+    links: [
+      {
+        name: "Lien vers Agrivitech",
+        href: "https://www.agrivitech.com/solutions/novacollecte"
+      }
+    ]
   }
 ];
